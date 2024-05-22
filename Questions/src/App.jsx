224282ae30601000961dfbb182d8b1b33f65e0dc
Justin <justinferreyra@gmail.com>
2024-05-22 16:34:06 -0300

    Fix las folder

--- Questions/src/App.jsx
+++ Questions/src/App.jsx
@@ -5,22 +5,22 @@ import './App.css'
 
 
 const questions = [{
-  "question" : "question 1",
-  "answer1" : ["answer A", true],
-  "answer2" : ["answer B", false],
-  "answer3" : ["answer C", false],
+  "question" : "question 1 an example",
+  "answer1" : ["answer A, this is true", true],
+  "answer2" : ["answer B, this is false", false],
+  "answer3" : ["answer C, this is false", false],
 },
 {
-  "question" : "question 2",
-  "answer1" : ["answer A", false],
-  "answer2" : ["answer B", false],
-  "answer3" : ["answer C", true],
+  "question" : "question 2 another example",
+  "answer1" : ["answer A, this is false", false],
+  "answer2" : ["answer B, this is false", false],
+  "answer3" : ["answer C, this is true", true],
 },
 {
-  "question" : "question 3",
-  "answer1" : ["answer A", false],
-  "answer2" : ["answer B", true],
-  "answer3" : ["answer C", false],
+  "question" : "question 3 last example",
+  "answer1" : ["answer A, this is false", false],
+  "answer2" : ["answer B, this is true", true],
+  "answer3" : ["answer C, this is false", false],
 },
 ]
 
@@ -29,6 +29,10 @@ const questions = [{
 // eslint-disable-next-line react/prop-types
 function Card({quizStatus, setQuizStatus, handleMore}) {
   let question = questions[quizStatus];
+  function questionNumbr() {
+    setQuizStatus(quizStatus + 1)
+  }
+
   return (
       <>
         <h1>The Questions Game</h1>
@@ -42,7 +46,7 @@ function Card({quizStatus, setQuizStatus, handleMore}) {
           ans3={question.answer3}
           >
           </AnsList>
-          <button onClick={()=>setQuizStatus(quizStatus + 1)}>NEXT</button>
+          <button className="next" onClick={questionNumbr}>NEXT</button>
         </ul>
     </>
   )
@@ -52,9 +56,9 @@ function AnsList({ ans1, ans2, ans3, handleMore}) {
 
   return(
     <> 
-    <li>{ans1[1] ? (<button className='correct' onClick={handleMore}>{ans1}</button>):(<button className='incorrect'onClick={()=> alert("bad choice")}>{ans1}</button>)}</li>
-    <li>{ans2[1] ? (<button className='correct' onClick={handleMore}>{ans2}</button>):(<button className='incorrect'onClick={()=> alert("bad choice")}>{ans2}</button>)}</li>
-    <li>{ans3[1] ? (<button className='correct' onClick={handleMore}>{ans3}</button>):(<button className='incorrect'onClick={()=> alert("bad choice")}>{ans3}</button>)}</li>
+    <li>{ans1[1] ? (<button className='correct' onClick={handleMore}>{ans1}</button>):(<button className='incorrect'onClick={()=> alert("wrong")}>{ans1}</button>)}</li>
+    <li>{ans2[1] ? (<button className='correct' onClick={handleMore}>{ans2}</button>):(<button className='incorrect'onClick={()=> alert("wrong")}>{ans2}</button>)}</li>
+    <li>{ans3[1] ? (<button className='correct' onClick={handleMore}>{ans3}</button>):(<button className='incorrect'onClick={()=> alert("wrong")}>{ans3}</button>)}</li>
     </>
     
   )
@@ -73,8 +77,9 @@ function App() {
   const [quizStatus , setQuizStatus] = useState(0);
   const [point , setPoint] = useState(0)
   function handleMore() {
-    setPoint( point + 1)
-    
+    if (point > quizStatus) {
+      return
+    }else setPoint( point + 1)
   }
   return (
     <>
